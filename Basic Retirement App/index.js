30b75ea6e62124ca331269fd4b302eef573e9141
gabelock <65908248+gabelock@users.noreply.github.com>
2020-08-03 14:58:11 -0300

    Basic Retirement App
    A basic and simple app for a retirement calc ( this is a 
    fictitious calculation, inside of the retirement process exist any others factors 
    to be considered).

---
+++ Basic Retirement App/index.js
@@ -0,0 +1,22 @@
+//Programa para calcular a aposentadoria de uma pessoa.
+
+ const nome = 'Joao'
+ const sexo = 'M'
+ const idade = 60
+ const contribuicao = 42
+
+ const aposentadoria = idade + contribuicao
+ //O tempo de contribuição mínimo para homens é de 35 anos e, para mulheres, 30 anos;
+if (sexo == "F") {
+  if (aposentadoria >= 85){
+    console.log(`${nome}, você está aposentada.`)
+  } else {
+    console.log(`${nome}, você não está aposentada.`)
+  }
+} else {
+  if (aposentadoria >= 95) {
+    console.log(`${nome}, você está aposentado.`)
+  } else {
+    console.log(`${nome}, você não está aposentado.`)
+  }
+}
